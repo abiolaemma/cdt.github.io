2cbd684c1d1ac2b3c624d530c9f3f1641d1b08af
Emmanuel <42983208+abiolaemma@users.noreply.github.com>
2022-11-14 13:11:15 -0500

    Add files via upload

---
+++ CDT/cdd.js
@@ -0,0 +1,36 @@
+
+// To declare a variable, we can use var or let
+
+var carName; 
+let carName;
+
+// Adding two numbers and return the result.  *
+
+const num1 =5;
+const num2=10;
+
+const sum= num1 + num2;
+
+// To write an array with the variable "fruits" and include four fruits. 
+var fruits1 = "Apple";
+var fruits2 = "Orange";
+var fruits3 = "Mango";
+var fruits4 = "Banana";
+
+// another method
+
+var fruits = new Array("Apple", "Orange", "Mango", "Banana");
+
+// To write an object that stores data
+
+let Hometown
+const Emmanuel={
+    firstnName:'Emmanuel',
+    lastName:'Ogundipe',
+    birthYear:2000,
+    jobTitle: 'Software Engineer',
+    Movie: `Designated survivor, Black Panther Transporter`
+    TodaysDate: `11/12/2022`
+    Hometown: `Iseyin`
+    
+    }
